b9fb748ab93a84f0f6140cf87b083e9adc5bb35a
Dor-B <dor46200@gmail.com>
2020-06-29 00:21:58 +0300

    Add map and filter

--- Stream.h
+++ Stream.h
@@ -7,23 +7,93 @@
 
 #include <vector>
 #include <map>
+#include <functional>
+#include <algorithm>
 
-using std::vector, std::map;
+using std::vector;
+using std::map;
+using std::function;
+using std::remove_if;
+using std::transform;
+
+using std::cout;
+using std::endl;
+
+template <typename ElemType>
+class Stream;
+
+template <typename ElemType>
+class FilterStream;
+
+template <typename OldType, typename NewType>
+class MapStream;
+
 template <typename ElemType>
 class Stream{
-    vector<ElemType> initialVector;
+public:
+    std::function<vector<ElemType*>()> compute;
 public:
     template <typename Container>
     static Stream of(Container& container){
-        return Stream(vector<ElemType>(container.begin(), container.end()));
+        return Stream<ElemType>(
+        [container]() -> vector<ElemType*> {
+            return vector<ElemType*>(container.begin(), container.end());
+        });
     }
 
 //    template <typename KeyType>
-//    static Stream of<map<KeyType, ElemType>> (map<KeyType, ElemType>& container){
-//        return Stream(vector<ElemType>(container.begin()))
+//    static Stream of(map<KeyType, ElemType>& container){
+//        return Stream<ElemType>([container]() -> vector<ElemType*> {
+//            vector <ElemType> v;
+//            for(typename map<KeyType, ElemType>::iterator it = container.begin(); it != container.end(); ++it ) {
+//                v.push_back( it->second );
+//            }
+//            return v;
+//        });
 //    }
+
+    Stream filter(std::function<bool(const ElemType*)> filterFunc){
+        return FilterStream<ElemType>(*this, filterFunc);
+    }
+
+    template <typename NewType>
+    Stream<NewType> map(std::function<NewType*(const ElemType*)> mapFunc){
+        return MapStream<ElemType, NewType>(*this, mapFunc);
+    }
+    Stream() = default;
 private:
-    Stream(vector<ElemType> initialVector): initialVector(initialVector){};
+    Stream(std::function<vector<ElemType*>()> compute): compute(compute){};
+};
+
+template <typename ElemType>
+class FilterStream: public Stream<ElemType> {
+public:
+    FilterStream(Stream<ElemType>& oldStream, std::function<bool(const ElemType*)> filterFunc)
+    : Stream<ElemType>(){
+        this->compute = [oldStream, filterFunc]()-> vector<ElemType*>{
+            vector<ElemType*> old = oldStream.compute();
+            vector<ElemType*> newV;
+            std::copy_if (old.begin(), old.end(), std::back_inserter(newV), filterFunc);
+            return newV;
+        };
+    }
+};
+
+template <typename OldType, typename NewType>
+class MapStream: public Stream<NewType> {
+public:
+    MapStream(Stream<OldType>& oldStream, std::function<NewType*(const OldType*)> mapFunc): Stream<NewType>(){
+        this->compute = [oldStream, mapFunc]() -> vector<NewType*>{
+            vector<OldType*> old = oldStream.compute();
+            vector<NewType*> newV;
+//            for(auto p:old){
+//                cout << *p << endl;
+//            }
+//            cout << "----------" << endl;
+            transform(old.begin(), old.end(), std::back_inserter(newV), mapFunc);
+            return newV;
+        };
+    }
 };
 
 #endif //OOP5_STREAM_H
--- main.cpp
+++ main.cpp
@@ -1,23 +1,35 @@
 #include <iostream>
-#include "MatrixOperations.h"
-#include "Printer.h"
-
+//#include "MatrixOperations.h"
+//#include "Printer.h"
+#include "Stream.h"
+#include <functional>
 using namespace std;
+class A{};
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-//    typedef X<> x;
-    typedef List<Int<1>, Int<2>, Int<3>> l1;
-    typedef List<
-            List<Int<1>, Int<2>, Int<3>>,
-            List<Int<0>, Int<1>, Int<0>>,
-            List<Int<0>, Int<1>, Int<0>>
-            > m1;
-    typedef List<
-            List<Int<3>, Int<2>, Int<1>>,
-            List<Int<1>, Int<0>, Int<1>>
-            > m2;
-    typedef typename Multiply<m2, m1>::result res;
-    Printer<res>::print(cout);
+    int array[10] = { 1, 2, 3, 2, 4, 6, 5, 7, 8, 9 };
+    std::vector<int*> intPointerVector;
+    for(int i = 0 ; i < 10 ; i++) intPointerVector.push_back(array + i);
+
+//    Stream<int> s = Stream<int>::of(intPointerVector).filter([](const int* val) { return (*val) > 2; });
+    auto s = Stream<int>::of(intPointerVector).map<int>([](const int* val){ return new int(-*val); })
+            .filter([](const int* val) { return (*val) < -2; });
+    for(auto p : s.compute()){
+        cout << *p << endl;
+    }
+//    std::cout << "Hello, World!" << std::endl;
+////    typedef X<> x;
+//    typedef List<Int<1>, Int<2>, Int<3>> l1;
+//    typedef List<
+//            List<Int<1>, Int<2>, Int<3>>,
+//            List<Int<0>, Int<1>, Int<0>>,
+//            List<Int<0>, Int<1>, Int<0>>
+//            > m1;
+//    typedef List<
+//            List<Int<3>, Int<2>, Int<1>>,
+//            List<Int<1>, Int<0>, Int<1>>
+//            > m2;
+//    typedef typename Multiply<m2, m1>::result res;
+//    Printer<res>::print(cout);
 //    static_assert(List<int>)
     return 0;
 }
