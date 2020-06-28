8170fd7cd0714f654d0730c42692dcb1c276e6eb
Dor-B <dor46200@gmail.com>
2020-06-28 18:45:57 +0300

    Add Add of matrixes

--- MatrixOperations.h
+++ MatrixOperations.h
@@ -5,4 +5,26 @@
 #ifndef OOP5_MATRIXOPERATIONS_H
 #define OOP5_MATRIXOPERATIONS_H
 
+#include "Utilities.h"
+
+template <typename Mat1, typename Mat2>
+struct Add{
+    typedef typename Mat1::next nextMat1Rows;
+    typedef typename Mat2::next nextMat2Rows;
+
+    typedef typename Mat1::head firstMat1Row;
+    typedef typename Mat2::head firstMat2Row;
+
+    typedef typename ListsAdd<firstMat1Row, firstMat2Row>::list firstRow;
+
+    typedef typename Add<nextMat1Rows, nextMat2Rows>::result BellowRows;
+
+    typedef typename PrependList<firstRow, BellowRows>::list result;
+};
+
+template <typename Row1, typename Row2>
+struct Add<List<Row1>, List<Row2>>{
+    typedef typename ListsAdd<Row1, Row2>::list firstRow;
+    typedef List<firstRow> result;
+};
 #endif //OOP5_MATRIXOPERATIONS_H
--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
-#include "Utilities.h"
+#include "MatrixOperations.h"
 #include "Printer.h"
 
 using namespace std;
 int main() {
     std::cout << "Hello, World!" << std::endl;
 //    typedef X<> x;
-    typedef List<Int<1>, Int<2>, Int<3>> l1;
-    typedef List<Int<3>, Int<2>, Int<1>> l2;
-    typedef typename ListsAdd<l1, l2>::list sum;
+    typedef List<
+            List<Int<1>, Int<2>, Int<3>>,
+            List<Int<0>, Int<1>, Int<0>>
+            > m1;
+    typedef List<
+            List<Int<3>, Int<2>, Int<1>>,
+            List<Int<1>, Int<0>, Int<1>>
+            > m2;
+    typedef typename Add<m1, m2>::result sum;
     Printer<sum>::print(cout);
 //    static_assert(List<int>)
     return 0;
