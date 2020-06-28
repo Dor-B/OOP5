a7681dacaa638f7276a67fcf108bd62065a26565
Dor-B <dor46200@gmail.com>
2020-06-28 20:30:29 +0300

    Starting part 2

---
+++ Stream.h
@@ -0,0 +1,16 @@
+//
+// Created by Dor on 6/28/2020.
+//
+
+#ifndef OOP5_STREAM_H
+#define OOP5_STREAM_H
+
+#include <vector>
+
+template <typename T>
+class Stream{
+public:
+//    static of()
+};
+
+#endif //OOP5_STREAM_H
