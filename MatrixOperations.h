4affa9b51293c4da53905ba1c4d26bdd93331666
Dor-B <dor46200@gmail.com>
2020-06-28 19:35:35 +0300

    Fix Multiply, Passes test

--- MatrixOperations.h
+++ MatrixOperations.h
@@ -6,9 +6,11 @@
 #define OOP5_MATRIXOPERATIONS_H
 
 #include "Utilities.h"
+#include "Transpose.h"
 
 template <typename Mat1, typename Mat2>
 struct Add{
+    static_assert(Mat1::size == Mat2::size, "Not the same rows number!");
     typedef typename Mat1::next nextMat1Rows;
     typedef typename Mat2::next nextMat2Rows;
 
@@ -36,12 +38,45 @@ struct Add<List<Row1>, List<Row2>>{
 
 template <typename Row, typename MatTranspose>
 struct MultRowByMatTranspose{
+    typedef typename MatTranspose::next nextMatCols;
 
+    typedef typename MatTranspose::head firstMatCol;
+
+    typedef typename VectorMult<Row, firstMatCol>::res scalar;
+
+    typedef typename MultRowByMatTranspose<Row, nextMatCols>::res RowResTail;
+
+    typedef typename PrependList<scalar, RowResTail >::list res;
 };
 
 template <typename Row, typename RowOfMat>
-struct MultRowByMatTranspose<Row, List<RowOfMat>{
+struct MultRowByMatTranspose<Row, List<RowOfMat>>{
+    typedef typename VectorMult<Row, RowOfMat>::res scalar;
+    typedef List<scalar> res;
+};
 
+template <typename Mat1, typename Mat2Transpose>
+struct _Multiply{
+    typedef typename Mat1::head firstMat1Row;
+    typedef typename Mat1::next nextMat1Row;
+
+    typedef typename MultRowByMatTranspose<firstMat1Row, Mat2Transpose>::res firstRow;
+
+    typedef typename _Multiply<nextMat1Row, Mat2Transpose>::result BellowRows;
+
+    typedef typename PrependList<firstRow, BellowRows>::list result;
+};
+
+template <typename Row1, typename Mat2>
+struct _Multiply<List<Row1>, Mat2>{
+    typedef typename MultRowByMatTranspose<Row1, Mat2>::res firstRow;
+    typedef List<firstRow> result;
+};
+
+template <typename Mat1, typename Mat2>
+struct Multiply {
+    typedef typename Transpose<Mat2>::matrix Mat2T;
+    typedef typename _Multiply<Mat1, Mat2T>::result result;
 };
 
 #endif //OOP5_MATRIXOPERATIONS_H
--- main.cpp
+++ main.cpp
@@ -6,16 +6,18 @@ using namespace std;
 int main() {
     std::cout << "Hello, World!" << std::endl;
 //    typedef X<> x;
+    typedef List<Int<1>, Int<2>, Int<3>> l1;
     typedef List<
             List<Int<1>, Int<2>, Int<3>>,
+            List<Int<0>, Int<1>, Int<0>>,
             List<Int<0>, Int<1>, Int<0>>
             > m1;
     typedef List<
             List<Int<3>, Int<2>, Int<1>>,
             List<Int<1>, Int<0>, Int<1>>
             > m2;
-    typedef typename Add<m1, m2>::result sum;
-    Printer<sum>::print(cout);
+    typedef typename Multiply<m2, m1>::result res;
+    Printer<res>::print(cout);
 //    static_assert(List<int>)
     return 0;
 }
